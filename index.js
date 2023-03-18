0baa73a7a8abe9152fb520c2ae4a25cd27fcc2ea
Naim Ali <naimalinaim1@gmail.com>
2023-03-18 15:33:50 +0600

    input parameter validate and function description added

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
-// first problem
+// input a number || calculate number and return result
 const mindGame = (num) => {
+  if (isNaN(num) || typeof num === "string") {
+    return "ERR: please input only number";
+  }
   num *= 3;
   num += 10;
   num /= 2;
   num -= 5;
   return num;
 };
-// console.log(mindGame(5))
-// console.log(mindGame(50))
-// console.log(mindGame(33))
-// second problem
+
+// input a string || string length is even return 'even' else return 'odd'
 const evenOdd = (str) => {
+  if (typeof str !== "string") {
+    return "ERR: please input only string";
+  }
   return str.length % 2 === 0 ? "even" : "odd";
 };
-// console.log(evenOdd('pHero'))
-// console.log(evenOdd('pHero '))
 
-// third problem
+// input a number || calculate number and return
 const isLGSeven = (num) => {
+  if (typeof num !== "number") {
+    return "ERR: please input only number";
+  }
   num -= 7;
   return num < 7 ? num : (num + 7) * 2;
 };
-// console.log(isLGSeven(6))
-// console.log(isLGSeven(-15))
-// console.log(isLGSeven(15))
 
-// fourth problem
+// input an array || return count all negative number
 const findingBadData = (numbers) => {
+  if (!Array.isArray(numbers)) {
+    return "ERR: please input only array";
+  }
   let badData = 0;
   numbers.forEach((num) => {
     num < 0 && badData++;
   });
   return badData;
 };
-// console.log(findingBadData([1, 2, 5]))
-// console.log(findingBadData([2, -5, -7, -13]))
-// console.log(findingBadData([-4, -9, -5, -33, -55]))
-// fifth problem
+
+// input third number || calculate gems to diamond and return
 const gemsToDiamond = (gems1, gems2, gems3) => {
+  const getType = gems1 + gems2 + gems3;
+  if (typeof getType !== "number" || isNaN(getType) || getType < 0) {
+    return "ERR: please input only positive number";
+  }
   gems1 *= 21;
   gems2 *= 32;
   gems3 *= 43;
   const total = gems1 + gems2 + gems3;
   return total >= 2000 ? total - 2000 : total;
 };
-
-// console.log(gemsToDiamond(1, 1, 1));
-// console.log(gemsToDiamond(20, 200, 50))
-// console.log(gemsToDiamond(100, 5, 1))
