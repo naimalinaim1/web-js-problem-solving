61b4c69bbbdc2693d7469c935dc107d065aa5d63
Naim Ali <naimalinaim1@gmail.com>
2023-03-18 15:00:19 +0600

    added README file

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
 // first problem
-const mindGame = () => {
-    /**
-     * input a number
-     * * 3
-     * + 10
-     * / 2 
-     * - 5
-     * return result number
-     */
-}
-
+const mindGame = (num) => {
+  num *= 3;
+  num += 10;
+  num /= 2;
+  num -= 5;
+  return num;
+};
+// console.log(mindGame(5))
+// console.log(mindGame(50))
+// console.log(mindGame(33))
 // second problem
-const evenOdd = () => {
-    /**
-     * input a string
-     * if string length is even return 'even'
-     * else return 'odd'
-     */
-}
+const evenOdd = (str) => {
+  return str.length % 2 === 0 ? "even" : "odd";
+};
+// console.log(evenOdd('pHero'))
+// console.log(evenOdd('pHero '))
 
 // third problem
-const isLGSeven = () => {
-    /**
-     * input a number
-     * - 7 
-     * if result less than 7 return result
-     * else result gater or equal than 7 return input * 2
-     */
-}
+const isLGSeven = (num) => {
+  num -= 7;
+  return num < 7 ? num : (num + 7) * 2;
+};
+// console.log(isLGSeven(6))
+// console.log(isLGSeven(-15))
+// console.log(isLGSeven(15))
 
 // fourth problem
-const findingBadData = () => {
-    /**
-     * input a array of number
-     * return negative number length
-     */
-}
-
+const findingBadData = (numbers) => {
+  let badData = 0;
+  numbers.forEach((num) => {
+    num < 0 && badData++;
+  });
+  return badData;
+};
+// console.log(findingBadData([1, 2, 5]))
+// console.log(findingBadData([2, -5, -7, -13]))
+// console.log(findingBadData([-4, -9, -5, -33, -55]))
 // fifth problem
-const gemsToDiamond = () => {
-    /**
-     * input -> three parameter
-     * first parameter ->  * 21
-     * second parameter -> * 32
-     * third parameter -> * 43
-     * add all parameter total
-     * if result gater than or equal 1000*2 return result - 1000*2
-     * else return result
-     */
-}
+const gemsToDiamond = (gems1, gems2, gems3) => {
+  gems1 *= 21;
+  gems2 *= 32;
+  gems3 *= 43;
+  const total = gems1 + gems2 + gems3;
+  return total >= 2000 ? total - 2000 : total;
+};
+
+// console.log(gemsToDiamond(1, 1, 1));
+// console.log(gemsToDiamond(20, 200, 50))
+// console.log(gemsToDiamond(100, 5, 1))
