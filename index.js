ba22d31eb172de15c7d7412610559cbdf0aba514
Naim Ali <naimalinaim1@gmail.com>
2023-03-18 14:25:11 +0600

    problem task

---
+++ index.js
@@ -0,0 +1,51 @@
+// first problem
+const mindGame = () => {
+    /**
+     * input a number
+     * * 3
+     * + 10
+     * / 2 
+     * - 5
+     * return result number
+     */
+}
+
+// second problem
+const evenOdd = () => {
+    /**
+     * input a string
+     * if string length is even return 'even'
+     * else return 'odd'
+     */
+}
+
+// third problem
+const isLGSeven = () => {
+    /**
+     * input a number
+     * - 7 
+     * if result less than 7 return result
+     * else result gater or equal than 7 return input * 2
+     */
+}
+
+// fourth problem
+const findingBadData = () => {
+    /**
+     * input a array of number
+     * return negative number length
+     */
+}
+
+// fifth problem
+const gemsToDiamond = () => {
+    /**
+     * input -> three parameter
+     * first parameter ->  * 21
+     * second parameter -> * 32
+     * third parameter -> * 43
+     * add all parameter total
+     * if result gater than or equal 1000*2 return result - 1000*2
+     * else return result
+     */
+}
